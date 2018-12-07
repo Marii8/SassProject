26c9af05aec6b44d76786a7694fd08ecd90088a4
Mariko Aburai <aburmari@gmail.com>
2018-12-07 13:08:59 +0900

    concept, productのレイアウト完成

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
-// $(function(){
-    
-// });
+console.log(1);
+
+$(function(){
+    console.log(2);
+    let arry = ["../img/plieauchocolat.jpg", "../img/shokupan.jpg", "../img/painauxpaisins.jpg", "../img/patatedouce.jpg", "../img/tourte.jpg", "../img/batarl.jpg", "../img/plieauchocolat.jpg",
+    ];
+
+    console.log(arry);
+
+
+    // for(let i = 0; i < arry.length; i++) {
+        console.log(arry[0]);
+
+    let list = window.$('#bread-list');
+    breadList.innerHTML('<img src="' + arry[0] + '/>');
+    list.append(breadList);
+
+
+
+    // }
+
+
+});
