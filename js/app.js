0c10fd5ca609532d2ae369991337b607cb157ca8
Mariko Aburai <aburmari@gmail.com>
2018-12-09 00:53:18 +0900

    画像にfilterをつけるjs

--- js/app.js
+++ js/app.js
@@ -4,10 +4,32 @@ $(function(){
     let arry = ["../img/plieauchocolat.jpg", "../img/shokupan.jpg", "../img/painauxpaisins.jpg", "../img/patatedouce.jpg", "../img/tourte.jpg", "../img/batarl.jpg", "../img/plieauchocolat.jpg",
     ];
 
-    for(let i = 0; i < arry.length; i++) {
-        let list = window.$('#bread-list');
-        let breadImg = $('<li>')
-            breadImg.html(`<img src="${arry[i]}" alt="breadImg[${i}]" class="bread-image" />`);
+    let list = window.$('#js-bread-list');
+    let breadImg;
+    let filter;
+    let i;
+
+    for(i = 0; i < arry.length; i++) {
+        breadImg = $('<li>')
+            breadImg.html(`<img src="${arry[i]}" alt="breadImg[${i}]" class="bread-image" id="breadImg${i}" />`);
         list.append(breadImg);
     }
- }); //_bakery.scss pastryの画像表示 ここまで
+//_bakery.scss pastryの画像表示 ここまで
+
+
+
+    $('#breadImg0').hover(
+        function() { //マウスカーソルが重なった時の処理
+            console.log('hovered!!');
+        filter = $('<div>');
+        filter.addClass('filter');
+            console.log(filter);
+            console.log(list);
+    $('#breadImg0').parent().append();
+    },
+        function() { //マウスカーソルが離れた時の処理
+            console.log('mouseleft!');
+        filter.toggle('filter');
+    });
+
+ });
